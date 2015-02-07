f2b7232749f1b7df4329385546fd49202d9f3507
Paulo Pires <pjpires@gmail.com>
2015-02-07 12:18:00 +0000

    Added Docker stuff.

---
+++ run_logstash_forwarder.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sed -i 's/LOGSTASH_SERVICE_HOST/$LOGSTASH_SERVICE_HOST/g' /logstash-forwarder/logstash-forwarder.conf
+sed -i 's/LOGSTASH_SERVICE_PORT/$LOGSTASH_SERVICE_PORT/g' /logstash-forwarder/logstash-forwarder.conf
+
+/logstash-forwarder/logstash-forwarder -config /logstash-forwarder/logstash-forwarder.conf
